de80944d13539574c8e31be2ac83793facb6435e
Tim McMaster <tim@mcmaster.id.au>
2018-10-07 20:56:24 +1100

    mgrated to using the tm-firebase component, and cleaned up code.

--- tm-generic-app.js
+++ tm-generic-app.js
@@ -22,6 +22,11 @@ import '@polymer/iron-pages/iron-pages.js';
 import '@polymer/iron-selector/iron-selector.js';
 import '@polymer/paper-icon-button/paper-icon-button.js';
 import '@polymer/paper-button/paper-button.js';
+import '@vaadin/vaadin-list-box/vaadin-list-box.js';
+import {} from 'tm-firebase';
+import '/node_modules/@polymer/iron-icon/iron-icon.js';
+import '/node_modules/@polymer/iron-icons/iron-icons.js';
+
 // import '../shared/my-icons.js';
 
 
@@ -90,10 +95,18 @@ class GenericApp extends PolymerElement {
             display:none;
         }
         
+        app-drawer {
+        -webkit-user-select: none;
+            /* Chrome/Safari */
+            -moz-user-select: none;
+            /* Firefox */
+            -ms-user-select: none;
+            /* IE10+ */
+        }
       </style>
       
-      <script src="https://cdn.firebase.com/libs/firebaseui/3.4.1/firebaseui.js"></script>
-      <link type="text/css" rel="stylesheet" href="https://cdn.firebase.com/libs/firebaseui/3.4.1/firebaseui.css" />
+      <!--<script src="https://cdn.firebase.com/libs/firebaseui/3.4.1/firebaseui.js"></script>-->
+      <!--<link type="text/css" rel="stylesheet" href="https://cdn.firebase.com/libs/firebaseui/3.4.1/firebaseui.css" />-->
     
       <app-location route="{{route}}" url-space-regex="^[[rootPath]]">
       </app-location>
@@ -101,12 +114,12 @@ class GenericApp extends PolymerElement {
       <app-route route="{{route}}" pattern="[[rootPath]]:page" data="{{routeData}}" tail="{{subroute}}">
       </app-route>
 
-      <!--<template is="dom-if" if="[[_goodToGo(user, readyToGo)]]">-->
-          <app-drawer-layout fullbleed="" narrow="{{narrow}}">
+          <app-drawer-layout fullbleed="" narrow="{{narrow}}" force-narrow="[[fullscreen]]">
             <!-- Drawer content -->
-            <app-drawer id="drawer" slot="drawer" swipe-open="[[narrow]]">
+            <app-drawer id="drawer" slot="drawer" swipe-open="[[narrow]]" transition-duration="1000">
               <app-toolbar>Menu</app-toolbar>
-              <iron-selector selected="[[page]]" attr-for-selected="name" class="drawer-list" role="navigation">
+              <tm-firebase class$="show-[[_loginRequired()]]" name="login" config="[[firebaseConfig]]" user="{{user}}"></tm-firebase>
+              <iron-selector selected="[[page]]" attr-for-selected="name" class$="drawer-list show-[[_goodToGo(user,readyToGo)]]" role="navigation">
                 <template is="dom-repeat" items="[[pages]]" as="page">
                     <a name="[[page.name]]" href="[[rootPath]][[page.name]]">[[page.title]]</a>
                 </template>
@@ -118,26 +131,17 @@ class GenericApp extends PolymerElement {
     
               <app-header slot="header" condenses="" reveals="" effects="waterfall">
                 <app-toolbar>
-                  <paper-icon-button icon="my-icons:menu" drawer-toggle=""></paper-icon-button>
+                  <paper-icon-button icon="menu" drawer-toggle=""></paper-icon-button>
                   <div id="maintitle" main-title="">[[title]]</div>
                 </app-toolbar>
               </app-header>
     
-              <iron-pages selected="[[page]]" attr-for-selected="name" role="main">
+              <iron-pages selected="[[page]]" attr-for-selected="name" role="main"> 
                 <slot name="page"></slot>
                 <my-view404 name="view404"></my-view404>
-                
-                
               </iron-pages>
             </app-header-layout>
           </app-drawer-layout>
-       <!--</template>-->
-       
-       <!--<template is="dom-if" if="[[_showLogin(user,readyToGo)]]">-->
-          <div id="login" class$="show-[[_showLogin(user,readyToGo)]]">
-             <div id="auth"></div>
-          </div>
-       <!--</template>-->
     `;
     }
 
@@ -150,6 +154,7 @@ class GenericApp extends PolymerElement {
             page: {
                 type: String,
                 reflectToAttribute: true,
+                value:'view2',
                 observer: '_pageChanged'
             },
             pagesMap: {
@@ -160,6 +165,10 @@ class GenericApp extends PolymerElement {
                 type: Array,
                 value: [],
                 observer: '_pagesChanged'
+            },
+            fullscreen: {
+                type: Boolean,
+                value: false
             },
             loginRequired: {
                 type: Boolean,
@@ -190,33 +199,6 @@ class GenericApp extends PolymerElement {
         };
     }
 
-    _showLogin(user, goodToGo) {
-        return this._loginRequired() && goodToGo;
-    }
-
-    _loginRequired() {
-        return (this.loginRequired && !this._loggedIn(this.user));
-    }
-
-    _goodToGo(user, readyToGo) {
-        const goodToGo = (!this.loginRequired || this._loggedIn(user)) && readyToGo === true;
-        return goodToGo;
-    }
-    _loggedIn(user) {
-        return (user !== undefined && user !== null && user.uid !== undefined);
-    }
-
-    _userChanged(user) {
-        console.log('User Changed: ', user);
-    }
-
-    _firebaseConfigChanged(firebaseConfig) {
-        console.log('Firebase is loader');
-        this.firebase = firebase;
-        this.firebase.initializeApp(firebaseConfig);
-        console.log('Firebase is initalised');
-
-    }
     static get observers() {
         return [
             '_routePageChanged(routeData.page)'
@@ -225,75 +207,41 @@ class GenericApp extends PolymerElement {
 
     ready() {
         super.ready();
-        this.readyToGo = true;
-
+        this.set('readyToGo', true);
         if (this._loginRequired()) {
-            this._login();
-        }
+            this.set('page', 'splash');
+            const self = this;
+            setTimeout(function() {
+                self.$.drawer.open();
+            }, 2000);
 
-        console.log('-------: DRAWER: ', this.$.drawer);
+        }
     }
 
-    _login() {
-        console.log('Creating the login screen');
+    _loginRequired() {
+        return (this.loginRequired && !this._loggedIn(this.user));
+    }
 
-        const self = this;
-        var uiConfig = {
-            callbacks: {
-                signInSuccessWithAuthResult: function(authResult, redirectUrl) {
-                    // User successfully signed in.
-                    // Return type determines whether we continue the redirect automatically
-                    // or whether we leave that to developer to handle.
-                    console.log(authResult);
-                    self.user = {
-                        uid: authResult.user.uid,
-                        name: authResult.user.displayName,
-                        email: authResult.user.email
-                    };
+    _goodToGo(user, readyToGo) {
+        const goodToGo = (!this.loginRequired || this._loggedIn(user)) && this.readyToGo === true;
+        return goodToGo;
+    }
 
-                    return false;
-                },
-                signInWithRedirect: function() {
-                    return false;
-                },
-                signInSuccess: function(currentUser, credential, redirectUrl) {
-                    console.log('Current User', currentUser);
-                    return false;
-                },
-                uiShown: function() {
-                    // The widget is rendered.
-                    // Hide the loader.
-                    // self.$.loader.style.display = 'none';
-                }
-            },
-            // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
-            signInFlow: 'popup',
-            signInSuccessUrl: 'localhost:8081',
-            signInOptions: [
-                // Leave the lines as is for the providers you want to offer your users.
-                firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-                //firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-                //firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-                //firebase.auth.GithubAuthProvider.PROVIDER_ID,
-                //firebase.auth.PhoneAuthProvider.PROVIDER_ID,
-                firebase.auth.EmailAuthProvider.PROVIDER_ID
-            ],
-            // Terms of service url.
-            tosUrl: '<your-tos-url>',
-            // Privacy policy url.
-            privacyPolicyUrl: '<your-privacy-policy-url>'
-        };
-
-        var ui = new firebaseui.auth.AuthUI(firebase.auth());
-        this.firebase.ui = ui;
-
-        // The start method will wait until the DOM is loaded.
-        console.log('Creating the login screen: ', ui);
-        ui.start(this.$.auth, uiConfig);
+    _loggedIn(user) {
+        const isLoggedIn = (user !== undefined && user !== null && user.uid !== undefined);
+        console.log('Login Checked: ', isLoggedIn);
+        return isLoggedIn;
+    }
+    _userChanged(user) {
+        console.log('User Changed: ', user);
+        if (user !== undefined && user.name !== undefined) {
+            this.set('page', this.pages[0].name);
+        }
     }
 
     _pagesChanged(pages) {
         const pagesMap = {};
+        pagesMap['splash'] = {name:'splash',title:'Splash'};
         this.pages.forEach(function(page) {
             pagesMap[page.name] = page;
         });
@@ -301,6 +249,7 @@ class GenericApp extends PolymerElement {
     }
 
     _routePageChanged(page) {
+        //const page = pagePath.substr(window.MyAppGlobals.rootPath.length);
         if (page) {
             this.page = (page in this.pagesMap ? page
                 : (this.pages.length > 0 ? this.pages[0].name : "view404"));
@@ -317,13 +266,9 @@ class GenericApp extends PolymerElement {
     }
 
     _pageChanged(page) {
-        if (page in this.pagesMap) {
-            if (this.pagesMap[page].lazy) {
-                const importName = './pages/my-' + page + '.js';
-                import(importName);
-            }
-        } else {
-            import('./pages/my-view404.js');
+        console.log('Page has changed: ' + page);
+        if (this.narrow === true) {
+            this.$.drawer.close();
         }
     }
 }
